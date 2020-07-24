7f0d901bbb502c28dabeae03031fed204435fae4
wvwnumam <wvwn@protonmail.ch>
2020-07-24 11:20:12 +0000

    Menambah daftar domain provinsi

--- app/controllers/statistik.php
+++ app/controllers/statistik.php
@@ -2,12 +2,26 @@
 
 class statistik extends Controller {
 
+    // Deklarasi Paramater API
+    public $key = ['bps_key' => 'ae16cc87c0398c4ab14d22fa99deed75'];
+    public $url = [
+        'covid_ind' => 'https://covid19.mathdro.id/api/countries/IDN',
+        'covid_prov' => 'https://data.covid19.go.id/public/api/prov.json',
+        'bps_domain' => 'https://webapi.bps.go.id/v1/api/domain'
+    ];
+    public $type = [
+        'all' => '&type=all', 
+        'prov' => '&type=prov', 
+        'kab' => '&type=kab', 
+        'provbykab' => '&type=provbykab'
+    ];
+
     // Fungsi GET JSON
-    public function getData($url, $key = NULL)
+    public function getData($alamat, $kunci = NULL, $arg = NULL)
     {
-        $url = $url . '?' . $key;
-        $curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_URL, $url);
+        $alamat = $alamat . '?key=' . $kunci . $arg ;
+        $curl = curl_init($alamat);
+        curl_setopt($curl, CURLOPT_URL, $alamat);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         $result = curl_exec($curl);
         curl_close($curl);
@@ -17,19 +31,22 @@ class statistik extends Controller {
     public function index()
     {
         // Kasus Covid-19 Indonesia
-        $kasus = $this->getData('https://covid19.mathdro.id/api/countries/IDN');
+        $kasus = $this->getData($this->url['covid_ind']);
 
         // Kasus Covid-19 Provinsi
-        $provinsi = $this->getData('https://data.covid19.go.id/public/api/prov.json');
+        $provinsi = $this->getData($this->url['covid_prov']);
 
+        // Daftar Domain Provinsi
+        $domain = $this->getData($this->url['bps_domain'], $this->key['bps_key'], $this->type['prov']);
+
+        // Daftar Variable View
         $data['judul'] = 'Daftar Statistik';
-
         $data['confirmed'] = $kasus['confirmed']['value'];
         $data['recovered'] = $kasus['recovered']['value'];
         $data['deaths'] = $kasus['deaths']['value'];
-        $data['lastUpdate'] = $kasus['lastUpdate'];
-        
+        $data['lastUpdate'] = $kasus['lastUpdate'];   
         $data['prov'] = $provinsi['list_data'];
+        $data['domain'] = $domain['data'][1];
 
         // $data['stat'] = $this->model('Statistik')->getAllStatistik();
         
--- app/views/statistik/index.php
+++ app/views/statistik/index.php
@@ -4,6 +4,7 @@
       <p class="lead">Sistem Informasi Pemerintah dan Data Statistik Dampak dari Covid-19</p>
       <hr class="my-4">
 
+
       <!-- Kasus Seluruh Indonesia -->
       <div class="card" style="width: 36rem;">
         <div class="card-header">
@@ -17,6 +18,7 @@
         </ul>
       </div>
       <br><br>
+
 
       <!-- Kasus Per-Provinsi -->
       <div class="row">
@@ -38,6 +40,23 @@
       </div>
 
       <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+    
+
+    <!-- Daftar domain provinsi-->
+    <div class="row">
+      <?php foreach($data['domain'] as $domain): ?>
+        <div class="col-3 mb-3">
+          <div class="card">
+            <div class="card-header">
+            <?= $domain['domain_name']; ?>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item"><?= $domain['domain_id']; ?></li>
+            </ul>
+          </div>
+        </div>
+      <?php endforeach; ?>
     </div>
 
 
