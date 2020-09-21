ac59b15e8968668d4078646041ea4f51ead72e59
rexushello123 <rexushello123@gmail.com>
2020-09-21 11:23:33 +0700

    tentang kami

--- app/views/tentang/index.php
+++ app/views/tentang/index.php
@@ -1,5 +1,78 @@
 <div class="container">
-    
-    <h1 class="mt-4">Tentang Aplikasi</h1>
-
+<div class="card-deck">
+  <div class="card" >
+      <div class="zyx">
+    <div class="card-body">
+        <div class="xyz">
+      <h1 class="card-title">260</h1>
+      <p class="card-text">PENGUNJUNG WEBSITE</p>
 </div>
+</div>
+    </div>
+  </div>
+  <div class="card">
+  <div class="zyx">
+    <div class="card-body">
+        <div class="xyz">
+      <h1 class="card-title"> 100</h1>
+      <p class="card-text">PENGGUNA</p>
+    </div>
+  </div>
+</div>
+</div>
+  <div class="card">
+  <div class="zyx">
+    <div class="card-body">
+        <div class="xyz">
+      <h1 class="card-title">84</h1>
+      <p class="card-text">USER TERSURVEI</p>
+    </div>
+  </div>
+</div>    
+</div>
+</div>
+
+
+    <div class="judul">
+    <h1 class="mt-4">Tim Kami</h1>
+    <p class="x">kami bekerja kreatif dengan melibatkan pengujian kegunaan</p>
+    <p class="x">untuk membuat aplikasi yang berguna untuk semua orang</p>
+    </div>
+    <div class="card-deck" >
+  <div class="card" >
+    <img class="card-img-top" src="img/pp2.jpg" class="image-circle"alt="Card image cap" >
+    <div class="card-body">
+      <h2 class="card-title">Ahmad N</h2>
+      <p class="p">Back-end</p>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="img/pp2.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h2 class="card-title">Bima S</h2>
+      <p class="p">Leader</p>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="img/pp2.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h2 class="card-title">Irgi A</h2>
+      <p class="p">Front-end + Assets</p>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="img/pp2.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h2 class="card-title">Tegar S</h2>
+      <p class="p">Front-end + Assets</p>
+      <p class="card-text">This is a wider card with srd has even longer content than the first to show that equal height action.</p>
+    </div>
+  </div>
+</div>
+</div>
+
+
