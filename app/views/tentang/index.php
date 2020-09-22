2b5dbc00c481ee54b5e0d129c97a4f04631ceacc
rexushello123 <rexushello123@gmail.com>
2020-09-22 19:54:05 +0700

    Tentang Kami

--- app/views/tentang/index.php
+++ app/views/tentang/index.php
@@ -1,78 +1,172 @@
 <div class="container">
-<div class="card-deck">
-  <div class="card" >
-      <div class="zyx">
-    <div class="card-body">
-        <div class="xyz">
-      <h1 class="card-title">260</h1>
-      <p class="card-text">PENGUNJUNG WEBSITE</p>
-</div>
-</div>
-    </div>
-  </div>
-  <div class="card">
-  <div class="zyx">
-    <div class="card-body">
-        <div class="xyz">
-      <h1 class="card-title"> 100</h1>
-      <p class="card-text">PENGGUNA</p>
-    </div>
-  </div>
-</div>
-</div>
-  <div class="card">
-  <div class="zyx">
-    <div class="card-body">
-        <div class="xyz">
-      <h1 class="card-title">84</h1>
-      <p class="card-text">USER TERSURVEI</p>
-    </div>
-  </div>
-</div>    
-</div>
-</div>
+  <!--  Data Provinsi -->
+    <div class="row justify-content-center">
+        <div class="col-lg-12">
+            <div align="center" class="jumbotron color-content text-align-center mb-4">
+                <div class="card-deck">
 
+    <div class="card border-0 rounded">
+        
+          <div class="card-body color-bg rounded text-center">
+              <h1 class="card-title color-blue-font">260</h1>
+                <p class="card-text color-light-font">PENGUNJUNG WEBSITE</p>
+            </div>
+    </div>
 
+  <div class="card border-0 rounded">
+        
+          <div class="card-body color-bg rounded text-center">
+              <h1 class="card-title color-blue-font">260</h1>
+                <p class="card-text color-light-font">PENGUNJUNG WEBSITE</p>
+            </div>
+  </div>
+
+    <div class="card border-0 rounded">
+        
+          <div class="card-body color-bg rounded text-center">
+              <h1 class="card-title color-blue-font">260</h1>
+                <p class="card-text color-light-font">PENGUNJUNG WEBSITE</p>
+            </div>
+    </div>
+</div>
+            </div>
+        </div>
+    </div>
+    <!--  Data Provinsi End -->
+  
+
+<div class="row">
+  <div class="col-lg-12">
     <div class="judul">
-    <h1 class="mt-4">Tim Kami</h1>
-    <p class="x">kami bekerja kreatif dengan melibatkan pengujian kegunaan</p>
-    <p class="x">untuk membuat aplikasi yang berguna untuk semua orang</p>
+      <h1 class="mt-3">Tim Kami</h1>
+      <p class="x">kami bekerja kreatif dengan melibatkan pengujian kegunaan</p>
+      <p class="x">untuk membuat aplikasi yang berguna untuk semua orang</p>
+      </div>
+</div>
+</div>
+<div class="row text-center">
+  <div class="col-lg-12">
+      <div class="card-deck mt-4">
+      <div class="row">
+      <div class="col-lg-3 mb-4">
+      <div class="card color-content color-light-font">
+          <img class="card-img-top rounded-circle profil" src="img/pp2.jpg" alt="Card image cap" >
+          <div class="card-body">
+            <h2 class="card-title">Ahmad N</h2>
+            <p class="p">Back-end</p>
+            <ul class="list-inline social-buttons text-center mt-3">
+              <li class="list-inline-item">
+                <a href="https://www.facebook.com" target="_blank">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="https://www.instagram.com" target="_blank">
+                  <i class="fab fa-instagram"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="https://www.github.com" target="_blank">
+                  <i class="fab fa-github"></i>
+                </a>
+              </li>
+            </ul>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+        </div>
+        
+    
+        <div class="col-lg-3 mb-4">
+        <div class="card color-content color-light-font">
+      <img class="card-img-top rounded-circle" src="img/pp2.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h2 class="card-title">Bima S</h2>
+        <p class="p">Leader</p>
+        <ul class="list-inline social-buttons text-center mt-3">
+              <li class="list-inline-item">
+                <a href="https://www.facebook.com" target="_blank">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="https://www.instagram.com" target="_blank">
+                  <i class="fab fa-instagram"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="https://www.github.com" target="_blank">
+                  <i class="fab fa-github"></i>
+                </a>
+              </li>
+            </ul>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>  
     </div>
-    <div class="card-deck" >
-  <div class="card" >
-    <img class="card-img-top" src="img/pp2.jpg" class="image-circle"alt="Card image cap" >
-    <div class="card-body">
-      <h2 class="card-title">Ahmad N</h2>
-      <p class="p">Back-end</p>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+
+    
+    <div class="col-lg-3 mb-4">
+    <div class="card color-content color-light-font">
+      <img class="card-img-top rounded-circle" src="img/pp2.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h2 class="card-title">Irgi A</h2>
+        <p class="p">Front-end + Assets</p>
+        <ul class="list-inline social-buttons text-center mt-3">
+              <li class="list-inline-item">
+                <a href="https://www.facebook.com/grosir.masker.140" target="_blank">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="https://www.instagram.com" target="_blank">
+                  <i class="fab fa-instagram"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="https://www.github.com" target="_blank">
+                  <i class="fab fa-github"></i>
+                </a>
+              </li>
+            </ul>
+        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+    </div>
+    
+    <div class="col-lg-3 mb-4">
+    <div class="card color-content color-light-font">
+      <img class="card-img-top rounded-circle" src="img/pp2.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h2 class="card-title">Tegar S</h2>
+        <p class="p">Front-end + Assets</p>
+        <ul class="list-inline social-buttons text-center mt-3">
+              <li class="list-inline-item">
+                <a href="https://www.facebook.com" target="_blank">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="https://www.instagram.com" target="_blank">
+                  <i class="fab fa-instagram"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="https://github.com/rexushello123" target="_blank">
+                  <i class="fab fa-github"></i>
+                </a>
+              </li>
+            </ul>
+        <p class="card-text">This is a wider card with srd has even longer content than the first to show that equal height action.</p>
+      </div>
+    </div>
     </div>
   </div>
-  <div class="card">
-    <img class="card-img-top" src="img/pp2.jpg" alt="Card image cap">
-    <div class="card-body">
-      <h2 class="card-title">Bima S</h2>
-      <p class="p">Leader</p>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    </div>
   </div>
 
-  <div class="card">
-    <img class="card-img-top" src="img/pp2.jpg" alt="Card image cap">
-    <div class="card-body">
-      <h2 class="card-title">Irgi A</h2>
-      <p class="p">Front-end + Assets</p>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
     </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="img/pp2.jpg" alt="Card image cap">
-    <div class="card-body">
-      <h2 class="card-title">Tegar S</h2>
-      <p class="p">Front-end + Assets</p>
-      <p class="card-text">This is a wider card with srd has even longer content than the first to show that equal height action.</p>
-    </div>
-  </div>
-</div>
+
+    </div>    
 </div>
 
 
