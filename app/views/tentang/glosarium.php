a85bd76c1fb3b826e95ea2f90001ec78593e69c5
rexushello123 <rexushello123@gmail.com>
2020-12-17 12:14:11 +0700

    Glosarium v1

---
+++ app/views/tentang/glosarium.php
@@ -0,0 +1,47 @@
+<div class="container">
+    <div class="col-lg-7 mb-4 color-light-font">
+        <h1>GLOSARIUM</h1>
+    </div>
+
+    <div class="col-md-12 mb-5">
+        <div class="card-body color-blue-bg rounded-top" style="height:4rem">
+        <h2 class="pl-1">Ekonomi</h2>
+		</div>
+        <table class="color-content rounded-bottom" rules="none">
+				<tr>
+					<td class="col-md-4 pl-4 text-primary">PRDB</td>
+					<td class="p-3 color-light-font">Produk domestik regional bruto adalah jumlah nilai tambah bruto yang timbul dari seluruh sektor perekonomian di daerah tersebut.</td>
+				</tr>
+                <tr>
+					<td class="col-md-3 pl-4 text-primary">APBD</td>
+					<td class="p-3 color-light-font">Anggaran Pendapatan dan Belanja Daerah, adalah rencana keuangan tahunan pemerintah daerah di Indonesia yang disetujui oleh Dewan Perwakilan Rakyat Daerah.</td>
+				</tr>
+                <tr>
+					<td class="col-md-3 pl-4 text-primary">Indeks Pembangunan Manusia</td>
+					<td class="p-3 color-light-font">Indeks Pembangunan Manusia atau Human Development Index adalah pengukuran perbandingan dari harapan hidup, melek huruf, pendidikan dan standar hidup.</td>
+				</tr>
+		</table> 
+	</div>
+
+	<div class="col-md-12 mb-5">
+        <div class="card-body color-blue-bg rounded-top" style="height:4rem">
+        <h2 class="pl-1">Umum</h2>
+		</div>
+        <table class="color-content rounded-bottom" rules="none">
+				<tr>
+					<td class="col-md-4 pl-4 text-primary">Statistika</td>
+					<td class="p-3 color-light-font">Sebuah ilmu yang mempelajari bagaimana cara merencanakan, mengumpulkan, menganalisis, lalu menginterpretasikan, dan akhirnya mempresentasikan data.</td>
+				</tr>
+                <tr>
+					<td class="col-md-3 pl-4 text-primary">Statistik</td>
+					<td class="p-3 color-light-font">Suatu kumpulan data yang berbentuk angka dan disusun dalam bentuk diagram dan/ atau tabel dimana isinya menjelaskan mengenai masalah tertentu.</td>
+				</tr>
+                <tr>
+					<td class="col-md-3 pl-4 text-primary">Survei</td>
+					<td class="p-3 color-light-font">Metode pengumpulan data primer dengan memberikan pertanyaan-pertanyaan kepada responden individu.</td>
+				</tr>
+		</table> 
+	</div>
+
+
+</div>
